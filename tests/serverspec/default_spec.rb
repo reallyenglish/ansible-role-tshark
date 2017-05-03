6423b2da3599dd03d9ef052956c53d29a16dc9d6
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-05-03 20:57:32 +0900

    support ubuntu

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -4,23 +4,41 @@ require "serverspec"
 package = "tshark"
 group   = "network"
 log_dir = "/var/log/tshark"
+dumpcap = "/usr/bin/dumpcap"
+dumpcap_mode = 755
 default_user = "root"
 default_group = "root"
 
 case os[:family]
+when "ubuntu"
+  group = "wireshark"
+  dumpcap_mode = 754
 when "redhat"
   package = "wireshark"
   group = "wireshark"
+  dumpcap = "/usr/sbin/dumpcap"
+  dumpcap_mode = 750
 when "openbsd"
   default_group = "wheel"
   group = "_wireshark"
+  dumpcap = "/usr/local/bin/dumpcap"
+  dumpcap_mode = 550
 when "freebsd"
   default_group = "wheel"
+  dumpcap = "/usr/local/bin/dumpcap"
+  dumpcap_mode = 2555
 end
 
 describe package(package) do
   it { should be_installed }
 end 
+
+describe file(dumpcap) do
+  it { should be_file }
+  it { should be_mode dumpcap_mode }
+  it { should be_owned_by default_user }
+  it { should be_grouped_into group }
+end
 
 describe user("vagrant") do
   it { should belong_to_group group }
